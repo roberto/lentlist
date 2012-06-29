68ec4822019c7b8c7187e865d8f6f7a7a267f971
Roberto Soares <roberto.tech@gmail.com>
2012-06-29 17:42:12 -0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lentlist::Application.config.secret_token = '59b687dd49c895cf194ced3b6cbe19e05a3fd9146c3dcfc56ae0039f0c7fdb0d71080113c84e55321066758aa45233befaf1270b27bdba680344fe32be3a6a43'
