565f029d35c17a50521d75ac929be6b7307a8441
Roberto Soares <roberto.tech@gmail.com>
2012-06-30 21:06:20 -0300

    added edit, archive and delete buttons

--- spec/requests/display_list_spec.rb
+++ spec/requests/display_list_spec.rb
@@ -2,16 +2,26 @@ require 'spec_helper'
 
 describe "DisplayList" do
   describe "GET /" do
-
     let(:items) { FactoryGirl.create_list(:item, 5)}
 
-    it "should display the current list" do
+    before do
       visit root_path
+    end
+
+    it "should display the current items (title and borrower)" do
       within('table') do
         items.each do |item|
           page.should have_content(item.title)
           page.should have_content(item.borrower)
         end
+      end
+    end
+
+    it "should display edit, delete and archive button for each item" do
+      all(:xpath, "//tbody/tr").each do |element|
+        element.should have_link('Edit')
+        element.should have_link('Archive')
+        element.should have_link('Delete')
       end
     end
   end
