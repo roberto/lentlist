f10d6750937e9671aa800035234769a19182fc82
Roberto Soares <roberto.tech@gmail.com>
2012-08-21 22:43:19 -0300

    coverage 100%

--- spec/requests/edit_items_spec.rb
+++ spec/requests/edit_items_spec.rb
@@ -1,32 +1,64 @@
 require 'spec_helper'
 
 describe "Edit item" do
-  describe "GET /items/:id/edit" do
-    let(:item) { FactoryGirl.create(:item) }
+  let(:item) { FactoryGirl.create(:item) }
 
+  before do
+    visit edit_item_path(item)
+  end
+
+  it "should load current data" do
+    within("fieldset") do
+      page.should have_field("I lent"), with: item.borrower
+      page.should have_field("my"), with: item.title
+    end
+  end
+
+  context "submitting valid item" do
     before do
-      visit edit_item_path(item)
-    end
-
-    it "should load current data" do
-      within("fieldset") do
-        page.should have_field("I lent"), with: item.borrower
-        page.should have_field("my"), with: item.title
-      end
-    end
-
-    it "should update data after submit" do
       within("fieldset") do
         fill_in "I lent", with: "Bruno"
         fill_in "my", with: "iPad"
         click_button('Update Item')
       end
+    end
 
+    it "should redirect to items list" do
       current_path.should == items_path
+    end
 
+    it "should update data after submit" do
       updated_item = Item.find(item)
       updated_item.borrower.should == "Bruno"
       updated_item.title.should == "iPad"
+    end
+
+    it "should info user about the success" do
+      find(".alert").should have_content("Item updated successfully.")
+    end
+  end
+
+  context "submitting invalid item" do
+    before do
+      within("fieldset") do
+        fill_in "I lent", with: "David"
+        fill_in "my", with: ""
+        click_button('Update Item')
+      end
+    end
+
+    it "should stay on the edit page" do
+      find("legend").should have_content("Edit Item")
+    end
+
+    it "should alert user about the error" do
+      find(".alert").should have_content("The item could not be updated.")
+    end
+
+    it "should not update data after submit" do
+      reloaded_item = Item.find(item)
+      reloaded_item.borrower.should == item.borrower
+      reloaded_item.title.should == item.title
     end
   end
 end
--- spec/requests/new_item_spec.rb
+++ spec/requests/new_item_spec.rb
@@ -1,31 +1,58 @@
 require 'spec_helper'
 
 describe "New item" do
-  describe "GET /items/new" do
+  before do
+    visit new_item_path
+  end
 
+  it "should load current data" do
+    within("fieldset") do
+      page.should have_selector(:xpath, "//input[@placeholder='John']")
+      page.should have_selector(:xpath, "//input[@placeholder='Cookbook']")
+    end
+  end
+
+
+  context "submitting valid item" do
     before do
-      visit new_item_path
-    end
-
-    it "should load current data" do
-      within("fieldset") do
-        page.should have_selector(:xpath, "//input[@placeholder='John']")
-        page.should have_selector(:xpath, "//input[@placeholder='Cookbook']")
-      end
-    end
-
-    it "should create item after submit" do
       within("fieldset") do
         fill_in "I lent", with: "Davis"
         fill_in "my", with: "Kindle"
         click_button('Create Item')
       end
+    end
 
+    it "should redirect to items list" do
       current_path.should == items_path
+    end
 
+    it "should create item after submit" do
       updated_item = Item.last
       updated_item.borrower.should == "Davis"
       updated_item.title.should == "Kindle"
+    end
+  end
+
+  context "submitting invalid item" do
+    before do
+      Item.delete_all
+      within("fieldset") do
+        fill_in "I lent", with: "Davis"
+        fill_in "my", with: ""
+        click_button('Create Item')
+      end
+    end
+
+    it "should stay on new item page" do
+      find("legend").should have_content("New Item")
+    end
+
+    it "should alert user about error" do
+      find('.alert').should have_content("The item could not be created.")
+    end
+
+    it "should create item after submit" do
+      Item.last.should be_nil
     end
   end
 end
