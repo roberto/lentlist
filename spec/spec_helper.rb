de3ef5bb84ba0feba518b6f0fbb32664dbc8f790
Roberto Soares <roberto.tech@gmail.com>
2012-11-20 21:41:13 -0300

    spec helper: database cleaner

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -23,28 +23,24 @@ RSpec.configure do |config|
     c.syntax = :expect
   end
 
-  config.mock_framework = :mocha
-
   config.include PageSteps, :type => :feature
   config.include FormSteps, :type => :feature
-  # ## Mock Framework
-  #
-  # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
-  #
-  # config.mock_with :mocha
-  # config.mock_with :flexmock
-  # config.mock_with :rr
 
-  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  # config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.before :each do
+    if Capybara.current_driver == :selenium
+      DatabaseCleaner.strategy = :truncation
+    else
+      DatabaseCleaner.strategy = :transaction
+    end
+    DatabaseCleaner.start
+  end
 
-  # If you're not using ActiveRecord, or you'd prefer not to run each of your
-  # examples within a transaction, remove the following line or assign false
-  # instead of true.
-  # config.use_transactional_fixtures = true
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+  
+  config.mock_with :mocha
 
-  # If true, the base class of anonymous controllers will be inferred
-  # automatically. This will be the default behavior in future versions of
-  # rspec-rails.
-  config.infer_base_class_for_anonymous_controllers = false
+  config.use_transactional_fixtures = false
+  config.infer_base_class_for_anonymous_controllers = true
 end
